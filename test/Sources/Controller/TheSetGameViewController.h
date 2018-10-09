b89b12509ebb95ef156e0b2a54a0f45d80dff146
Alordead <spealcy@gmail.com>
2018-10-09 18:28:33 +0300

    Create new MVC in project

---
+++ test/Sources/Controller/TheSetGameViewController.h
@@ -0,0 +1,17 @@
+//
+//  TheSetGameViewController.h
+//  test
+//
+//  Created by Александр Попов on 09.10.2018.
+//  Copyright © 2018 Александр Попов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TheSetGameViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/Sources/Controller/TheSetGameViewController.m
@@ -0,0 +1,32 @@
+//
+//  TheSetGameViewController.m
+//  test
+//
+//  Created by Александр Попов on 09.10.2018.
+//  Copyright © 2018 Александр Попов. All rights reserved.
+//
+
+#import "TheSetGameViewController.h"
+
+@interface TheSetGameViewController ()
+
+@end
+
+@implementation TheSetGameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
